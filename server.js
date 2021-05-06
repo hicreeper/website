434415b629441035941cabf3ec44b51d90088b9c
hicreeper <sb457831@gmail.com>
2021-05-06 17:11:10 +0800

    test1

--- server.js
+++ server.js
@@ -34,20 +34,3 @@ let myApp = http.createServer(function (req, res) {
 myApp.listen(PORT);
 
 console.log("Server listening 3000....")
-/*var http = require('http');
-var fs = require('fs');
-function onRequest(request, response){
-	response.writeHead(200, {'Content-Type': 'text/html'});
-	fs.readFile('./index.html', null, function(error, data){
-		if(error){
-			response.writeHead(404);
-			response.write('File not found!');
-		}
-		else{
-			response.write(data);
-		}
-		response.end();
-	})
-}
-
-http.createServer(onRequest).listen(8000);*/
